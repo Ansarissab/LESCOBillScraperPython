be343aa3a25a2c699b3cd2e4157029fa9b0e8466
Ansarissab <zahidensari116@gmail.com>
2023-11-27 01:48:12 +0500

    Improved it to download multiple bills

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 URL = 'http://www.lesco.gov.pk:36269/Modules/CustomerBill/CheckBill.asp'
 IMAGE_NAME = 'captcha_image.png'
-customer_id = '1234'
+customer_ids = ['1','2','3']
 
 chromium_path = subprocess.check_output(['which', 'chromium']).decode('utf-8').strip()
 chrome_options = webdriver.ChromeOptions()
@@ -19,41 +19,41 @@ chrome_options.binary_location = chromium_path
 # chrome_options.add_argument('--headless')
 chrome_options.add_argument('--kiosk-printing')
 
+for customer_id in customer_ids:
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(URL)
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get(URL)
+    driver.find_element(By.NAME, 'form2').find_element(By.NAME, 'txtCustID').send_keys(customer_id)
+    driver.find_element(By.NAME, 'form2').find_element(By.NAME, 'btnViewMenu').click()
+    time.sleep(3)
+    img_element = driver.find_element(By.CSS_SELECTOR, '#ContentPane img')
+    screenshot_path = IMAGE_NAME
+    img_element.screenshot(screenshot_path)
+    captcha_text = ''
+    print('Screenshot of the image captured successfully.')
+    image = Image.open(screenshot_path)
 
-driver.find_element(By.NAME, 'form2').find_element(By.NAME, 'txtCustID').send_keys(customer_id)
-driver.find_element(By.NAME, 'form2').find_element(By.NAME, 'btnViewMenu').click()
-time.sleep(5)
-img_element = driver.find_element(By.CSS_SELECTOR, '#ContentPane img')
-screenshot_path = IMAGE_NAME
-img_element.screenshot(screenshot_path)
-captcha_text = ''
-print('Screenshot of the image captured successfully.')
-image = Image.open(screenshot_path)
+    text_extraction_complete = False
+    while not text_extraction_complete:
+        try:
+            captcha_text = pytesseract.image_to_string(image)
+            text_extraction_complete = True
+        except Exception as e:
+            pass
 
-text_extraction_complete = False
-while not text_extraction_complete:
-    try:
-        captcha_text = pytesseract.image_to_string(image)
-        text_extraction_complete = True
-    except Exception as e:
-        pass
+    print('Text extracted from the image:', captcha_text)
+    image.close()
+    os.remove(IMAGE_NAME)
+    input_field = driver.find_element(By.CLASS_NAME, 'billform').find_element(By.NAME, 'code')
+    input_field.send_keys(captcha_text)
+    time.sleep(3)
+    html_content = driver.page_source
+    # driver.save_screenshot('final_screenshot.png')
 
-print('Text extracted from the image:', captcha_text)
-image.close()
-os.remove(IMAGE_NAME)
-input_field = driver.find_element(By.CLASS_NAME, 'billform').find_element(By.NAME, 'code')
-input_field.send_keys(captcha_text)
-time.sleep(5)
-html_content = driver.page_source
-# driver.save_screenshot('final_screenshot.png')
+    print_link = driver.find_element(By.CSS_SELECTOR, '#printPageButton a')
+    print_link.click()
+    time.sleep(3)
+    driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.ENTER)
+    time.sleep(3)
 
-print_link = driver.find_element(By.CSS_SELECTOR, '#printPageButton a')
-print_link.click()
-time.sleep(5)
-driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.ENTER)
-time.sleep(5)
-
-driver.quit()
+    driver.quit()
